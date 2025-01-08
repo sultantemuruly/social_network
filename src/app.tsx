715f060a489462186f83ed195160589fa39337dc
sultantemuruly <sultantemuruly@gmail.com>
2025-01-08 11:10:40 +0500

    basic setup

---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+import {Routes, Route} from "react-router-dom"
+
+const App = () => {
+  return 
+  <main className="flex h-screen">
+    <Routes>
+      {/* public routes */}
+
+      {/* private routes */}
+    </Routes>
+  </main>
+};
+export default App;
